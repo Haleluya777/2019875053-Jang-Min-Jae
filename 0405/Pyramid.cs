47acf733d84724d8be0527a9d7c0f78fec9d6841
Honolulu <88095893+Haleluya777@users.noreply.github.com>
2022-06-23 00:10:08 +0900

    Create Pyramid.cs

---
+++ 0405/Pyramid.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace namespaceA
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("      *");
+            Console.WriteLine("     ***");
+            Console.WriteLine("    *****");
+            Console.WriteLine("   *******");
+            Console.WriteLine("  *********");
+            Console.WriteLine(" ***********");
+            Console.WriteLine("*************");
+        }
+    }
+}
