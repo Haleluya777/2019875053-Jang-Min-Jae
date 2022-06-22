b30c0e80ea981a5014ede9aaec2bf84092e85d8f
Honolulu <88095893+Haleluya777@users.noreply.github.com>
2022-06-23 00:04:36 +0900

    Create DelegateCallApp.cs

---
+++ 0405/DelegateCallApp.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace DelegateCallApp
+{
+    delegate void DelegateOne();
+    delegate void DelegateTwo(int i); 
+
+    class DelegateClass
+
+    {
+        public void MethodA()
+        {
+            Console.WriteLine("In the DelegateClass.MethodA ...");
+        }
+        public void MethodB(int i)
+        {
+            Console.WriteLine("DelegateClass.MethodB, i = " + i);
+        }
+    }
+
+    class Program
+    {
+        public static void Main()
+        {
+            DelegateClass obj = new DelegateClass();
+            DelegateOne d1 = new DelegateOne(obj.MethodA);
+            DelegateTwo d2 = new DelegateTwo(obj.MethodB);
+
+            d1();
+            d2(10);
+        }
+    }
+}
